ac100fd82a80ca6b389efc74667e21d9b9992b20
Roshni-Bala <Roshni-Bala@users.noreply.github.com>
2021-10-02 01:08:28 +0530

    validate v1.0

--- app/src/main/java/com/example/test_roshni/FeedbackFormActivity.java
+++ app/src/main/java/com/example/test_roshni/FeedbackFormActivity.java
@@ -4,26 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.Statement;
+//import android.os.AsyncTask;
+//import static com.example.test_roshni.MainActivity.pass;
+//import static com.example.test_roshni.MainActivity.url;
+//import static com.example.test_roshni.MainActivity.user;
 
-import static com.example.test_roshni.MainActivity.pass;
-import static com.example.test_roshni.MainActivity.url;
-import static com.example.test_roshni.MainActivity.user;
 
-
-public class FeedbackFormActivity extends AppCompatActivity{
+public class FeedbackFormActivity extends AppCompatActivity {
     Button btn_submit1;
     EditText usernametext, userphonetext, useremailtext;
     public String user_name, user_phone, user_email;
-    public boolean success;
+//    public boolean success;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +36,25 @@ public class FeedbackFormActivity extends AppCompatActivity{
         userphonetext = findViewById(R.id.user_phone1);
         useremailtext = findViewById(R.id.user_email1);
 
-        btn_submit1.setOnClickListener(new View.OnClickListener(){
+        btn_submit1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                success = false;
+            public void onClick(View view) {
+//                success = false;
                 user_name = String.valueOf(usernametext.getText());
                 user_phone = String.valueOf(userphonetext.getText());
                 user_email = String.valueOf(useremailtext.getText());
-//                ConnectMySql connectMySql_db = new ConnectMySql();
+
+//                ConnectMySql connectMySql_db = new ConnectMySql(); //db connection jdbc
 //                connectMySql_db.execute("");
-                if(usernametext!=null){
+
+                if (user_name.length() != 0 && user_phone.length()!=0 && user_email.length()!=0) {
                     Intent intent2 = new Intent(FeedbackFormActivity.this, enterfb.class);
+                    Toast.makeText(FeedbackFormActivity.this, "Welcome, " + user_name, Toast.LENGTH_SHORT).show();
                     startActivity(intent2);
-                }
-                else{
-                    Toast.makeText(FeedbackFormActivity.this, "Please fill in the details.", Toast.LENGTH_SHORT).show();
+                } else if (user_name.length() == 0 || user_phone.length()==0 || user_email.length()==0) {
+                    Toast.makeText(FeedbackFormActivity.this, "Please enter details.", Toast.LENGTH_SHORT).show();
+                } else {
+                    System.out.println("**Error**");
                 }
 
             }
@@ -57,41 +62,47 @@ public class FeedbackFormActivity extends AppCompatActivity{
 
 
     }
-    private class ConnectMySql extends AsyncTask<String, Void, String> {
-        String res = "";
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            Toast.makeText(FeedbackFormActivity.this, "Please wait...", Toast.LENGTH_SHORT).show();
-
+    //TODO: check how to validate using this function below
+        public static boolean isValidEmail(CharSequence user_email) {
+            return (!TextUtils.isEmpty(user_email) && Patterns.EMAIL_ADDRESS.matcher(user_email).matches());
         }
 
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection con = DriverManager.getConnection(url, user, pass);
-                System.out.println("Databaseection success");
-
-                String result = "Database Connection Successful\n";
-                Statement st = con.createStatement();
-                int rs = st.executeUpdate("INSERT INTO `feedbacktable`(`name`, `email`, `phone`) VALUES('"+user_name+"','"+user_email+"','"+user_phone+"');");
-
-                res = result;
-            } catch (Exception e) {
-                e.printStackTrace();
-                res = e.toString();
-            }
-            return res;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            if(success){
-                Toast.makeText(FeedbackFormActivity.this, "Submitted.", Toast.LENGTH_SHORT).show();
-
-            }
-        }
-    }
 }
+//    private class ConnectMySql extends AsyncTask<String, Void, String> {
+//        String res = "";
+//
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//            Toast.makeText(FeedbackFormActivity.this, "Please wait...", Toast.LENGTH_SHORT).show();
+//
+//        }
+//
+//        @Override
+//        protected String doInBackground(String... params) {
+//            try {
+//                Class.forName("com.mysql.jdbc.Driver");
+//                Connection con = DriverManager.getConnection(url, user, pass);
+//                System.out.println("Databaseection success");
+//
+//                String result = "Database Connection Successful\n";
+//                Statement st = con.createStatement();
+//                int rs = st.executeUpdate("INSERT INTO `feedbacktable`(`name`, `email`, `phone`) VALUES('"+user_name+"','"+user_email+"','"+user_phone+"');");
+//
+//                res = result;
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                res = e.toString();
+//            }
+//            return res;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String result) {
+//            if(success){
+//                Toast.makeText(FeedbackFormActivity.this, "Submitted.", Toast.LENGTH_SHORT).show();
+//
+//            }
+//        }
+
