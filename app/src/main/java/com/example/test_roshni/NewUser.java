76d84eb30e7e483263afccd1074e5cba0e46486b
Roshni-Bala <Roshni-Bala@users.noreply.github.com>
2021-10-04 10:45:22 +0530

    Firebase Database Initiation

---
+++ app/src/main/java/com/example/test_roshni/NewUser.java
@@ -0,0 +1,10 @@
+package com.example.test_roshni;
+
+public class NewUser {
+    private String u_name;
+    private String u_email;
+    private String u_phone;
+    private String u_feedback;
+    private int u_rating;
+
+}
