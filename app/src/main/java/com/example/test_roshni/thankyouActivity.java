c299a14ea9594d9e04d98b8892d3d98202a698d2
Roshni-Bala <Roshni-Bala@users.noreply.github.com>
2021-10-04 22:46:19 +0530

    rating Update

--- app/src/main/java/com/example/test_roshni/thankyouActivity.java
+++ app/src/main/java/com/example/test_roshni/thankyouActivity.java
@@ -1,5 +1,8 @@
 package com.example.test_roshni;
 
+import static java.lang.Math.round;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,15 +10,55 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class thankyouActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.DecimalFormat;
+import java.util.Map;
+
+public class thankyouActivity extends AppCompatActivity {
+    private DatabaseReference avgdb;
+    float rateavg;
+    private TextView total_avg_rating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thankyou);
         Button btn_homepage, btn_k_web, btn_k_topgg, btn_k_git;
+        total_avg_rating = findViewById(R.id.txt_avg_rate);
+        avgdb = FirebaseDatabase.getInstance().getReference().child("NewUserFeedback");
+        avgdb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int sum = 0;
+                int count=0;
+                for(DataSnapshot ds : snapshot.getChildren()){
+                    Map<String, Object> map = (Map<String, Object>) ds.getValue();
+                    Object rate = map.get("u_rate");
+                    int rValue = Integer.parseInt(String.valueOf(rate));
+                    sum += rValue;
+                    count++;
+
+                }
+                DecimalFormat df = new DecimalFormat("#.##");
+                rateavg = (float)sum/count;
+                total_avg_rating.setText(String.valueOf(df.format(rateavg)));
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         btn_homepage = findViewById(R.id.btn_homepage);
         btn_k_web = findViewById(R.id.btn_k_web);
         btn_k_topgg = findViewById(R.id.btn_k_topgg);
